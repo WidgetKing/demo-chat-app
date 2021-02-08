1559297b35e3e72f7a0dcfdef9857e06096cce09
WidgetKing <tristan.ward@infosemantics.com.au>
2021-02-08 13:44:40 +0800

    Initial Commit

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+const http = require("http").createServer();
+
+const io = require("socket.io")(http, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", `${socket.id.substr(0, 2)} said ${message}`);
+  });
+});
+
+const port = process.env.PORT || 8080;
+
+http.listen(port, () => console.log(`listening on http://localhost:${port}`));
